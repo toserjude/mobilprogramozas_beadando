098a607f9e3a35539e27c7b732b519c2dfd7bb19
Tősér Judit <toser.jude@gmail.com>
2018-12-21 17:09:18 +0100

    initial and basics

---
+++ app/src/main/java/com/example/lenovo/memorygame/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lenovo.memorygame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.btn1);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/memorygame/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.lenovo.memorygame;
+
+public class SecondActivity {
+}
